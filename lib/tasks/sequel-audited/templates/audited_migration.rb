f6c55ded9b0cad1d5a6b64a2f18dbfd54491d071
Joakim Nylén <me@jnylen.nu>
2017-08-23 04:14:36 +0200

    Update db template for polymorphic

--- lib/tasks/sequel-audited/templates/audited_migration.rb
+++ lib/tasks/sequel-audited/templates/audited_migration.rb
@@ -1,23 +1,17 @@
 Sequel.migration do
   # created by sequel-audited gem
 
-  up do
+  change do
     create_table(:audit_logs) do
       primary_key :id
-      String    :model_type
-      Integer   :model_pk
-      String    :model_ref
+      String    :associated_type
+      Integer   :associated_id
       String    :event
       String    :changed,     text: true
       Integer   :version,     default: 0
-      Integer   :user_id
-      String    :username
-      String    :user_type,   default: 'User'
+      Integer   :modifier_id
+      String    :modifier_type,   default: 'User'
       DateTime  :created_at
     end
-  end
-
-  down do
-    drop_table :audit_logs
   end
 end
